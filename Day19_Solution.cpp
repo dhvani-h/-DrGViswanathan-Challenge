d8d9b87b994eb926f5a329937ddbc34187de717e
dhvani-h <harinkheredhvani@gmail.com>
2026-06-07 22:19:12 +0530

    Create Day19_Solution.cpp

---
+++ Day19_Solution.cpp
@@ -0,0 +1,20 @@
+/*
+Problem: Domino Piling
+Platform: Codeforces
+Difficulty: Easy
+Approach: Math
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int M, N;
+    cin >> M >> N;
+
+    cout << (M * N) / 2;
+
+    return 0;
+}
