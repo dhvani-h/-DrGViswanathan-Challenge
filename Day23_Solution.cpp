6d6fc954c2b57d9b57f6805fbd7605399b10fcbb
dhvani-h <harinkheredhvani@gmail.com>
2026-06-07 22:26:50 +0530

    Create Day23_Solution.cpp

---
+++ Day23_Solution.cpp
@@ -0,0 +1,20 @@
+/*
+Problem: Elephant
+Platform: Codeforces
+Difficulty: Easy-Medium
+Approach: Greedy / Math
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    cout << (x + 4) / 5;
+
+    return 0;
+}
